7e1346375ca958055be8ab82dd9453970e278456
minsung Joh <jms5049@naver.com>
2018-07-15 23:22:53 +0900

    insertion sort 02 - 02

--- insertion sort/example02.c
+++ insertion sort/example02.c
@@ -4,6 +4,41 @@ int arr[10000];
 int size=0;
 int i=0;
 
+void insertionSort(int *A, int n) {
+	int j,k,key = 0;
+	size = sizeof(arr);
+	size = size / 4;
+	A[i] = n;
+	
+	
+		for (k = 1; k < size; k++) {
+			key = A[k];
+			j = k - 1;
+			if (key == NULL) {			
+				break;
+			}
+			while (j >= 0 && A[j] > key) {
+				A[j + 1] = A[j];
+				j = j - 1;
+			}
+			j += 1;
+			A[j] = key;
+		}
+	
+	arr;
+	i++;
+}
+
+void print() {
+	int i = 0;
+	size = sizeof(arr);
+	size = size / 4;
+	for (i = 0; i < size; i++) {
+		printf("%d", arr[i]);
+		puts("");
+	}
+}
+
 void main() {
 
 	int score = 0;

694f32e11b2e276e19a3b1c02cc0eb5bad2bc38c
minsung Joh <jms5049@naver.com>
2018-07-16 23:35:48 +0900

    merge sort 01 - 01

---
+++ merge sort/example01.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+void mergeSort(int arrayM[], int left, int right);
+void merge(int arrayM[], int left, int mid, int right);
+
+int main()
+{
+	int i, count = 0, numbers = 0;
+	int *arrayM;
+
+	arrayM = (int*)malloc(sizeof(int));					
+
+	FILE *file;
+	file = fopen("test_10000.txt", "r");				
+
+	if (file) {
+		while (fscanf(file, "%d", &numbers) > 0) {
+			*(arrayM + count) = numbers;
+			count++;								
+			arrayM = (int *)realloc(arrayM, sizeof(int)*(count + 1));
+		}
+		fclose(file);								
+	}
+
+	mergeSort(arrayM, 0, count - 1);
+
+	printf("Sorted Array:\n");
+	for (i = 0; i < count; i++)
+		printf("%d\n", arrayM[i]);
+
+	free(arrayM);
+	return 0;
+}

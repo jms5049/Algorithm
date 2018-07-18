9647e5048ac8ca5cec16c6681e7caa6ff8c2be2e
minsung Joh <jms5049@naver.com>
2018-07-18 23:46:17 +0900

    merge sort 03 - 02

--- merge sort/example03.c
+++ merge sort/example03.c
@@ -37,3 +37,52 @@ int main()
 	free(arraySort);
 	return 0;
 }
+
+
+
+void mergeSort(int arraySort[], int left, int right)
+{
+	int mid;
+
+	if ( ( (right - left) + 1 ) <= 8) {
+		insertionSort(arraySort, left, right);
+	}
+
+	else if (left<right) {
+		mid = (left + right) / 2;
+		mergeSort(arraySort, left, mid);
+		mergeSort(arraySort, mid + 1, right);
+		merge(arraySort, left, mid, right);
+	}
+
+}
+
+void merge(int arraySort[], int left, int mid, int right)
+{
+	
+	int *array_left, *array_right;
+	int n1, n2, i, j, k;
+
+	n1 = mid - left + 1;
+	array_left = (int*)malloc(sizeof(int)*n1);
+
+	n2 = right - mid;
+	array_right = (int*)malloc(sizeof(int)*n2);
+
+	for (i = 0; i<n1; i++)
+		array_left[i] = arraySort[left + i];
+
+	for (j = 0; j<n2; j++)
+		array_right[j] = arraySort[mid + j + 1];
+	array_left[i] = INT_MAX;
+	array_right[j] = INT_MAX;
+	i = 0;
+	j = 0;
+	for (k = left; k <= right; k++) {
+		if (array_left[i] <= array_right[j])
+			arraySort[k] = array_left[i++];
+		else
+			arraySort[k] = array_right[j++];
+	}
+
+}

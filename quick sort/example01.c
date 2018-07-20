74d400dc2290ae523e25f080da61778b38aac3d8
minsung Joh <jms5049@naver.com>
2018-07-21 00:03:02 +0900

    naive quick sort 01 - 01

---
+++ quick sort/example01.c
@@ -0,0 +1,49 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//void quickSort(int *QuickArray, int p, int r);
+//void swap(int *QuickArray, int one, int two);
+//int partition(int *QuickArray, int p, int r);
+//
+//
+//
+//int main() {
+//	int i,numbers, count = 0;
+//	int *QuickArray;
+//	QuickArray = (int *)malloc(sizeof(int) );
+//	FILE *file;
+//	file = fopen("test1.txt", "r");				
+//	
+//	if (file) {
+//		while (fscanf(file, "%d", &numbers) > 0) {
+//			*(QuickArray + count) = numbers;
+//			count++;								
+//			QuickArray = (int *)realloc(QuickArray, sizeof(int)*(count + 1));
+//		}
+//		fclose(file);								
+//	}
+//
+//
+//
+//	quickSort(QuickArray, 0 , count-1);
+//
+//	printf("Quicksorted Array: \n");	
+//	for (i = 0; i < count; i++)
+//		printf("%d\t", QuickArray[i]);
+//
+//	free(QuickArray);
+//	return 0;
+//}
+//
+//void quickSort(int *QuickArray, int p, int r) {
+//	int q = 0;	
+//
+//	if (p < r) {
+//		q = partition(QuickArray, p, r);
+//		quickSort(QuickArray, p, q - 1);
+//		quickSort(QuickArray, q + 1, r);
+//	}
+//}
+//

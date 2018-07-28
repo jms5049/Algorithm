4d6d04f73462db7ff5d6fbaf06585654ba3f5113
minsung Joh <jms5049@naver.com>
2018-07-29 00:31:04 +0900

    bst - 01

---
+++ BST/BST.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+typedef struct TREENODE {
+	int data;
+	struct TREENODE *right;
+	struct TREENODE *left;
+	struct TREENODE *parent;
+}TREENODE;
+
+TREENODE* treeInsertRecursive(TREENODE *T, int z);
+//TREENODE *treeInsertIterative(TREENODE *T, int z);
+TREENODE *treeSearchRecursive(TREENODE *T, int k);
+TREENODE *treeSearchIterative(TREENODE *T, int k);
+TREENODE *treeSuccessor(TREENODE *T);
+TREENODE *treePredecessor(TREENODE *T);
+TREENODE *treeMin(TREENODE *T);
+TREENODE *treeMax(TREENODE *T);
+TREENODE* buildBST(TREENODE *T, int *A, int count);
+void treeDelete(TREENODE *T, int z);
+void transplant(TREENODE *T, TREENODE *u, TREENODE *v);
+void printTree(TREENODE *T);
+void insertionSort(int *A, int p, int r);
+void swap(int *A, int p, int r);
+int findmedian(int *A, int p, int r);
+
+TREENODE *root;
+int main() {
+	int i, numbers,input,count=0;
+	int *dataInput;
+	dataInput = (int *)malloc(sizeof(int));
+	FILE *file;
+	file = fopen("µ¥ÀÌÅÍ.txt", "r");				
+
+	if (file) {
+		while (fscanf(file, "%d", &numbers) > 0) {
+			*(dataInput + count) = numbers;
+			count++;								
+			dataInput = (int *)realloc(dataInput, sizeof(int)*(count + 1));
+		}
+		fclose(file);								
+	}
+
+
+	TREENODE *BST = NULL;
+	BST = buildBST(BST, dataInput, count);
+	//printf("\nEnter the value to Delete in BST : ");
+	//scanf("%d", &input);
+	//treeSearchIterative(BST, input);			
+	//treeDelete(BST, input);					
+	puts("");
+	//printTree(BST);							
+	puts("");
+	treeSuccessor(BST);
+	treePredecessor(BST);
+	puts("");
+
+
+	return 0;
+}
+

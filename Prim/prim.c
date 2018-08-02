f9dfbd81f43baaf5349f69b73f2c6251d8a9fc32
minsung Joh <jms5049@naver.com>
2018-08-02 21:27:33 +0900

    min heap

--- Prim/prim.c
+++ Prim/prim.c
@@ -75,3 +75,72 @@
 //	}
 //}
 //
+
+//
+//void min_heapify(VERTEX *A, int i) {
+//	int l, r, smallest;
+//	VERTEX temp ;
+//
+//	l = left(i);
+//	r = right(i);
+//	if (l < heapSize && A[l].key < A[i].key)
+//		smallest = l;
+//	else
+//		smallest = i;
+//
+//	if (r < heapSize && A[r].key < A[smallest].key)
+//		smallest = r;
+//
+//	if (smallest != i) {
+//		temp = A[i];
+//		A[i] = A[smallest];
+//		A[smallest] = temp;
+//
+//		min_heapify(A, smallest);
+//	}
+//}
+//
+//int parent(int i) {
+//	return floor(i / 2);
+//}
+//
+//int left(int i) {
+//	return 2 * i + 1;
+//}
+//
+//int right(int i) {
+//	return 2 * i + 2;
+//}
+//
+//int heap_extract_min(VERTEX *A) {
+//	int min_value;
+//
+//	min_value = A[0].index;
+//	A[0] = A[heapSize-1];
+//	heapSize--;
+//	min_heapify(A, 0);
+//	return min_value;
+//}
+//
+//void min_heap_decrease_key(VERTEX *A, int x, int key) {
+//	VERTEX temp;
+//	if (key > A[x].key) {
+//		printf("new key is bigger than current key");
+//	}
+//	else {
+//		A[x].key = key;
+//		while (x > 0 && A[parent(x)].key > A[x].key) {
+//
+//			temp = A[x];
+//			A[x] = A[parent(x)];
+//			A[parent(x)] = temp;
+//
+//			x = parent(x);
+//		}
+//	}
+//}
+//
+//void min_heap_insert(VERTEX *A, VERTEX v) {
+//	A[heapSize++]=v;
+//	min_heap_decrease_key(A, heapSize-1, v.key);
+//}

cf7c50c47e09e5220cac61aa629fd3ef94f85627
minsung Joh <jms5049@naver.com>
2018-08-13 23:02:05 +0900

    main

--- Chain order/Chain_order.c
+++ Chain order/Chain_order.c
@@ -29,3 +29,37 @@
 //
 //}
 //
+//void matrixChainOrder(int* p) {
+//	int **m = (int **)malloc(sizeof(int*)*matrixNo);
+//	int **s = (int **)malloc(sizeof(int*)*matrixNo);
+//	int i,j,k,l,q;
+//
+//	for (i = 0; i < matrixNo; i++) {
+//		m[i] = (int *)malloc(sizeof(int) * matrixNo);
+//		s[i] = (int *)malloc(sizeof(int) * matrixNo);
+//	}
+//
+//	for (i = 0; i < matrixNo; i++) {
+//		m[i][i] = 0;
+//	}
+//
+//	for (l = 2; l <= matrixNo; l++) {
+//		for (i = 0; i < matrixNo-l+1; i++) {
+//			j = i + l - 1;
+//			m[i][j] = INT_MAX;
+//			for (k = i; k < j; k++) {
+//				
+//				q = m[i][k] + m[k+1][j] + p[i]*p[k+1]*p[j+1];
+//				if (q < m[i][j]) {
+//					m[i][j] = q;
+//					s[i][j] = k;
+//				}
+//			}
+//		}
+//	}
+//
+//	puts("");
+//	printOptimalParens(s, 0, matrixNo-1);
+//	printf("\n\ncost: %d\n", m[0][matrixNo - 1]);
+//}
+//
